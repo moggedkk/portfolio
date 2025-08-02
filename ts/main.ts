fad9032c41699bab9262e90d939edf387da6a318
Adrian Strassnig <adrianstrassnig2@gmail.com>
2025-08-02 16:29:38 +0200

    Converting project to typescript - By: Adrian

---
+++ ts/main.ts
@@ -0,0 +1,14 @@
+let text:string = "a computer science student with a passion for tech and innovation.";
+var element = document.getElementById("typewriter");
+var i = 0;
+function type() {
+    if (i < text.length) {
+        var char = text.charAt(i);
+        if(element != null){
+            element.innerHTML += char;
+        }
+        i++;
+        setTimeout(type, 50);
+    }
+}
+window.onload = type;
